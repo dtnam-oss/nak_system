b58f6ec495d7f485d208ddf400a105e6543d8ffd
dtnam-oss <dtnam@nakvn.com>
2026-01-13 11:14:05 +0700

    chore: Force Vercel to clear build cache with dynamic buildId

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Force rebuild - clear cache
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
 };
 
 export default nextConfig;
